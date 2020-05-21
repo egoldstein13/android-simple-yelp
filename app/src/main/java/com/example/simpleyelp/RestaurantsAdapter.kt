24163b997c7f298a7925980f5430c719e3803414
egoldstein13 <esther.goldstein13@gmail.com>
2020-05-21 16:54:25 -0700

    Extend to include Yelp stars and logo

--- app/src/main/java/com/example/simpleyelp/RestaurantsAdapter.kt
+++ app/src/main/java/com/example/simpleyelp/RestaurantsAdapter.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -30,7 +31,18 @@ class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaur
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(restaurant: YelpRestaurant) {
             itemView.tvName.text = restaurant.name
-            itemView.ratingBar.rating = restaurant.rating.toFloat()
+            when(restaurant.rating.toFloat()) {
+                in 0.0..0.49 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_0))
+                in 0.5..1.24 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_1))
+                in 1.25..1.74 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_1_half))
+                in 1.75..2.24 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_2))
+                in 2.25..2.74 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_2_half))
+                in 2.75..3.24 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_3))
+                in 3.25..3.74 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_3_half))
+                in 3.75..4.24 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_4))
+                in 4.25..4.74 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_4_half))
+                in 4.75..5.0 -> itemView.yelpStars.setImageDrawable(getDrawable(context,R.drawable.stars_small_5))
+            }
             itemView.tvNumReviews.text = "${restaurant.numReviews} Reviews"
             itemView.tvAddress.text = restaurant.location.address
             itemView.tvCategory.text = restaurant.categories[0].title
@@ -39,6 +51,7 @@ class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaur
             Glide.with(context).load(restaurant.imageUrl).apply(RequestOptions().transforms(
                 CenterCrop(), RoundedCorners(20)
             )).into(itemView.imageView)
+            itemView.yelpLogo.setImageDrawable(getDrawable(context,R.drawable.yelp_logo))
         }
     }
 }
